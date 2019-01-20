f516f750daa04b2425567e2de33ae5da68ea5a3b
Nalin28 <nalinmunshi@gmail.com>
2019-01-20 19:31:09 +0530

    variables

--- variables.scala
+++ variables.scala
@@ -8,3 +8,16 @@ var d = 12.3f
 
 val x = {val a :Int = 200 ; val b : Int = 300 ; a+b}
 
+or
+
+val x = {
+	| val a : Int = 500
+	| val b : Int = 600
+	| a+b
+    |}
+
+ //if we have a list of millions of numbers but we dont use it then it would be a waste, for that we have a lazy declaration
+
+ lazy val x = 500//Int = <lazy> memory allocated only when the variable is in use
+
+ 
