84fb50da74c6149cdbd21dbaf674cf79c9d7194f
Nalin28 <nalinmunshi@gmail.com>
2019-01-20 20:14:11 +0530

    first program

--- hello_world.scala
+++ hello_world.scala
@@ -1,5 +1,6 @@
 
 object HelloWorld{//is a class which already has an instance so you cant call a new keyword in this object
-	def main(args: Array[String])
+	def main(args: Array[String]){
 		println("Hello, world!")
+	}
 }
